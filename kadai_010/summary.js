f276bab2b2f0a2e64b40f15eeb371b3a64102ab0
fukuhara0114 <fukuhara0114@gmail.com>
2024-03-21 11:03:57 +0900

    animation01

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -1,1 +1,19 @@
+$(()=>{
+
+    $('#change-color').on('click', ()=>{
+        $('#target').css('color', 'red');
+    });
+
+    $('#change-text').on('click', ()=>{
+        $('#target').text('Hello!');
+    });
+
+    $('#fade-out').on('click', ()=>{
+        $('#target').fadeOut();
+    });
+    
+    $('#fade-in').on('click', ()=>{
+       $('#target').fadeIn(); 
+    });
+});
 
